ea2377a6cf51fcffc70ccb3f2285bc485e507f0a
La Pyae <tolapyae@outlook.com>
2024-11-07 15:16:28 +0630

    Major Update in HelloJava with new admin class

--- HelloJava.java
+++ HelloJava.java
@@ -1,4 +1,7 @@
 // Default constructor
+import java.util.List;
+import java.util.Scanner;
+import java.util.ArrayList;
 
 class Book {
     private String name;
@@ -6,14 +9,6 @@ class Book {
     private double price;
     private int stock;
 
-
-    // default constructor for book class
-    public Book() {
-        this.name = "Untitled";
-        this.author = "Unknown";
-        this.price = 0.0;
-        this.stock = 0;
-    }
 
     // parameterized constructor for book class
     public Book(String name, String author, double price, int stock) {
@@ -23,30 +18,84 @@ class Book {
         this.stock = stock;
     }
 
-    public void displayBook() {
-        System.out.println("Name: " + name);
-        System.out.println("Author: " + author);
-        System.out.println("Price: $" + price);
-        System.out.println("Stock: " + stock);
+    //getter for each
+    public String getName() {
+        return this.name;
+    }
+    public String getAuthor() {
+        return this.author;
+    }
+    public double getPrice() {
+        return this.price;
+    }
+    public int getStock() {
+        return this.stock;
     }
 
     
 }
 
-// main 1.0 display default constructor
+// admin interaction
+class Admin {
+    private List<Book> books;
+
+    public Admin() {
+        this.books = new ArrayList<>();
+    }
+
+    // method to add book
+    public void addBook(String name, String author, double price, int stock){
+        Book book = new Book(name, author, price, stock);
+        books.add(book);
+
+    }
+
+    // method to view newly added book
+    public void viewAddedBook() {
+            if(books.isEmpty()) {
+                System.out.println("No Books Added Yet.");
+            } else {
+            System.out.printf("%-5s %-30s %-20s%n", "No.", "Title", "Author");
+            System.out.println("-------------------------------------------------");
+
+            int count = 1;
+            for (Book book: books) {
+                System.out.printf("%-5d %-30s %-20s%n", count, book.getName(), book.getAuthor());
+                count++;
+            }
+            System.out.println("------------------------------------------------------");
+            System.out.println("TOTAL BOOKS: " + (count - 1));
+                }
+            }
+        
+    }
+
+
+
+
+
+// major update java
 
 public class HelloJava {
 
     public static void main(String[] args) {
-        Book book = new Book();
+        Scanner scanner = new Scanner(System.in);
+        Admin admin = new Admin();
+        System.out.print("Title: ");
+        String name = scanner.nextLine();
+        System.out.print("Author: ");
+        String author = scanner.nextLine();
+        System.out.print("Price: $");
+        double price = scanner.nextDouble();
+        System.out.print("Stock: ");
+        int stock = scanner.nextInt();
+        admin.addBook(name, author, price, stock);
+        System.out.println("Book Successfully Added.");
+        scanner.nextLine();
+        scanner.close();
 
-        // for default constructor
-        book.displayBook();
-        System.out.println("\n");
+        admin.viewAddedBook();
 
-
-        //for parameterized constructor
-        Book book2 = new Book("Republic", "Plato", 34.5, 4);
-        book2.displayBook();
+        
     }
 }
